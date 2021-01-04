6520048cfaab00605794a58b2a45a380123361de
DROGOU Laurent <ldrogou@gmail.com>
2021-01-05 00:20:01 +0100

    :tada: initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ldrogou/goflix
+
+go 1.15
+
+require (
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.6
+)
---
+++ store.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+//Store interface
+type Store interface {
+	Open() error
+	Close() error
+	GetMovies() ([]*Movie, error)
+}
+
+type dbStore struct {
+	db *sqlx.DB
+}
+
+var schema = `
+CREATE TABLE IF NOT EXISTS movie
+(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT,
+	release_date TEXT,
+	duration INTEGER,
+	trailer_url TEXT
+)
+`
+
+func (store *dbStore) Open() error {
+	db, err := sqlx.Connect("sqlite3", "goflix.db")
+	if err != nil {
+		return err
+	}
+	log.Println("Connected to DB")
+	db.MustExec(schema)
+
+	store.db = db
+
+	return nil
+}
+
+func (store *dbStore) Close() error {
+	return store.db.Close()
+}
